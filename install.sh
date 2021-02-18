f62543943e7fdd605c40fcfd79f2e06afb3e864e
Pedro Leal <pedro.leal@shopify.com>
2021-02-18 10:52:54 -0500

    Add install script for spin

---
+++ install.sh
@@ -0,0 +1,15 @@
+if [ $SPIN ]; then
+  if ! command -v rcm &> /dev/null; then
+    sudo apt-get install -y rcm
+  fi
+
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
+
+  if ! command -v tig &> /dev/null; then
+    sudo apt-get install -y tig
+  fi
+
+  rcup -d dotfiles -x install.sh
+fi
